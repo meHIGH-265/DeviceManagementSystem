ee061d97cee35ce60f09b10b16c745bfa16a47aa
Mihai Secosan <secosanmihaisebastian@gmail.com>
2026-04-04 16:02:27 +0300

    Created a fresh asp.net project and added 2 domain classes matching the tables in the database (User, Device)

---
+++ backend/DeviceManagementSystem/Domain/Device.cs
@@ -0,0 +1,27 @@
+﻿namespace DeviceManagementSystem.Domain
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Manufacturer { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public string OS { get; set; } = null!;
+
+        public string? OSVersion { get; set; }
+
+        public string? Processor { get; set; }
+
+        public string? RAM { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? AssignedUserId { get; set; }
+
+        public User? AssignedUser { get; set; }
+    }
+}
---
+++ backend/DeviceManagementSystem/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿namespace DeviceManagementSystem.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+
+        public string Location { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+    }
+}
